07ee1cbec712a474b7a0f5d8fc14c849ff0fa95d
luckys-devv <ljgarnica78@gmail.com>
2025-04-24 19:51:14 -0300

    Carpetas y configuraciones publicas

--- src/controllers/climaController.js
+++ src/controllers/climaController.js
@@ -1,23 +1,44 @@
 const axios = require("axios");
 
 const getClima = async (req, res) => {
+
+  try {
     
-    try {
-        const { lat, lng } = req.query;
+    const { lat, lng } = req.query;
 
-        if (!lat || !lng) {
-            return res.status(400).json({ error: "Se requieren latitud y longitud" });
-        }
-
-        const API_URL = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lng}&current_weather=true&daily=temperature_2m_max,temperature_2m_min,precipitation_probability_max&timezone=auto`;
-
-        const response = await axios.get(API_URL);
-        res.json(response.data);
-
-    } catch (error) {
-        console.error("Error obteniendo el clima:", error.message);
-        res.status(500).json({ error: "Error al obtener el clima" });
+    if (!lat || !lng) {
+      return res
+        .status(400)
+        .json({ error: "Debe ingresar datos en latitud o longitud" });
     }
+
+    const API_URL = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lng}&current_weather=true&daily=temperature_2m_max,temperature_2m_min,precipitation_probability_max&timezone=auto`;
+
+    const response = await axios.get(API_URL);
+
+    if (!response.data) {
+      throw new Error("La respuesta desde la API, esta sin datos");
+    }
+
+    res.json(response.data);
+
+  } catch (error) {
+
+    if (error.response) {
+      res.status(error.response.status).json({
+        error: "La API ha generado un error",
+        details: error.response.data,
+      });
+    } else if (error.request) {
+      res.status(503).json({
+        error: "La solicitud ha generado un error de conexion con la API",
+        details: error.request.data,
+      });
+    } else {
+      res.status(500).json({ error: "Error interno en el servidor" });
+    }
+
+  }
 };
 
 module.exports = { getClima };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 const express = require("express"); // Manejamos las peticiones HTTP
 const cors = require("cors"); // Permitimos que nuestro front haga peticiones
+const path = require("path");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use('/public', express.static(path.join(__dirname, '..', 'public')));
 // Importamos las rutas del clima
 const climaRoutes = require("./routes/climaRoutes");
 app.use("/api", climaRoutes); // Prefijo "/api" para mantener orden
 
 const PORT = 3000;
 app.listen(PORT, () => {
+    
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
+
 });
 
 
