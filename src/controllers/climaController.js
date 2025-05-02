aa941450b46c3c6f925f22d6cae81fe3ed57a59c
luckys-devv <ljgarnica78@gmail.com>
2025-05-02 10:13:11 -0300

    Validacion de la Rpta de la API

--- src/controllers/climaController.js
+++ src/controllers/climaController.js
@@ -1,30 +1,36 @@
 const axios = require("axios");
 
 const getClima = async (req, res) => {
-
   try {
-    
     const { lat, lng } = req.query;
 
+    /* =====Validacion de parametros===== */
     if (!lat || !lng) {
       return res
         .status(400)
         .json({ error: "Debe ingresar datos en latitud o longitud" });
     }
 
+    /* =====Llamada a la api en otro hilo===== */
     // const API_URL = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lng}&current_weather=true&daily=temperature_2m_max,temperature_2m_min,precipitation_probability_max&timezone=auto`;
     const API_URL = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lng}&daily=weather_code,temperature_2m_max,temperature_2m_min,precipitation_probability_max&current=temperature_2m,apparent_temperature,is_day,precipitation,weather_code,wind_speed_10m,wind_direction_10m,relative_humidity_2m&timezone=auto`;
 
     const response = await axios.get(API_URL);
 
+    /* =====Valido que no sea un rpta vacia===== */
     if (!response.data) {
       throw new Error("La respuesta desde la API, esta sin datos");
     }
 
-    res.json(response.data);
+    // Verifico que me devuelva realmente un objeto JSON y no un array de objetos.
+    if (Array.isArray(response.data)) {
+      res.json(response.data[0]);
+    } else {
+      res.json(response.data);
+    }
 
+    // res.json(response.data);
   } catch (error) {
-
     if (error.response) {
       res.status(error.response.status).json({
         error: "La API ha generado un error",
@@ -38,7 +44,6 @@ const getClima = async (req, res) => {
     } else {
       res.status(500).json({ error: "Error interno en el servidor" });
     }
-
   }
 };
 
